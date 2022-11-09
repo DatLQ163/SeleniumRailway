beba991ec6fb1c6f1728bae86d555d52de31e31a
DatLQ163 <lequocdat163@gmail.com>
2022-11-09 11:38:59 +0700

    update 11/09

--- Seleniumlevel1/src/main/java/Common/Constant.java
+++ Seleniumlevel1/src/main/java/Common/Constant.java
@@ -1,16 +1,15 @@
 package Common.Constant;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Constant {
     public static WebDriver WEBDRIVER;
-    public static final String RAILWAY_URL = "http://railwayb2.somee.com/Page/HomePage.cshtml";
+    public static final String RAILWAYURL = "http://railwayb2.somee.com/Page/HomePage.cshtml";
     public static final String USERNAME = "quocdat@gmail.com";
     public static final String INVALIDUSERNAME = "abc";
     public static final String REGISTERUSERNAME = "Dat"+(int)(Math.random()*1000)+"@gmail.com";
     public static final String PASSWORD = "123456789";
     public static final String CONFIRMPASSWORD ="123456789";
     public static final String PASSPORTNUMBER = "abcd1234";
-    public static final int DEPARTDATE = (int)(Math.random()*31+27);
+    public static final int DEPART_DATE = (int)(Math.random()*31+27);
 }
